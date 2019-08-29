58582b0a46d809e61b1c2fb265993f9e3ff2957b
kirill <kbibik@jaguarlandrover.com>
2019-08-29 09:22:53 +0100

    refactoring

--- src/main/scala/playlistoptimizer/optimizer/Optimizer.scala
+++ src/main/scala/playlistoptimizer/optimizer/Optimizer.scala
@@ -6,7 +6,7 @@ import playlistoptimizer.domain.{Key, Playlist}
 import scala.util.Random
 
 sealed trait Optimizer {
-  def optimize(implicit random: Random): Playlist
+  def optimize(implicit rnd: Random): Playlist
 }
 
 object Optimizer {
@@ -15,13 +15,13 @@ object Optimizer {
     half.zip(population.filterNot(half.contains))
   }
 
-  private[optimizer] def mutate(pl: Playlist)(implicit random: Random): Playlist =
-    pl.map(_.swap(random.nextInt(pl.size), random.nextInt(pl.size)))
+  private[optimizer] def mutate(pl: Playlist)(implicit rnd: Random): Playlist =
+    pl.map(_.swap(rnd.nextInt(pl.size), rnd.nextInt(pl.size)))
 
-  private[optimizer] def crossover(pl1: Playlist, pl2: Playlist)(implicit random: Random): Playlist = pl1.zipWith(pl2)((s1, s2) => {
+  private[optimizer] def crossover(pl1: Playlist, pl2: Playlist)(implicit rnd: Random): Playlist = pl1.zipWith(pl2)((s1, s2) => {
     val half = s1.size / 2
-    val point1: Int = random.nextInt(half)
-    val point2: Int = random.nextInt(half) + half
+    val point1: Int = rnd.nextInt(half)
+    val point2: Int = rnd.nextInt(half) + half
     val (l, m, r) = s1.splitInThree(point1, point2)
     l ++ s2.filter(m.contains) ++ r
   })
@@ -32,17 +32,17 @@ object Optimizer {
 case class GeneticAlgorithmOptimizer(initialPlaylist: Playlist, populationSize: Int, iterations: Int, mutationFactor: Double) extends Optimizer {
   import playlistoptimizer.optimizer.Optimizer._
 
-  private val initialPopulation: List[Playlist] = List.fill(populationSize)(randomizedPlaylist)
-
-  override def optimize(implicit random: Random): Playlist =
+  override def optimize(implicit rnd: Random): Playlist = {
+    val initialPopulation: List[Playlist] = List.fill(populationSize)(randomizedPlaylist)
     (0 until populationSize)
       .foldLeft(initialPopulation)((currentPopulation, _) => singleIteration(currentPopulation))
       .head
+  }
 
-  private def singleIteration(population: List[Playlist])(implicit random: Random): List[Playlist] = {
+  private def singleIteration(population: List[Playlist])(implicit rnd: Random): List[Playlist] = {
     val newPopulation = distributeInPairs(population)
       .flatMap { case (p1, p2) => List(crossover(p1, p2), crossover(p2, p1)) }
-      .map(m => if (random.nextDouble < mutationFactor) Optimizer.mutate(m) else m)
+      .map(m => if (rnd.nextDouble < mutationFactor) Optimizer.mutate(m) else m)
 
     (newPopulation ++ population).sortBy(evaluate).take(populationSize)
   }
