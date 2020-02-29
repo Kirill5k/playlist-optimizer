12d74053b6ae795985bfceae2c47121727b32ce7
kirill5k <immotional@aol.com>
2020-02-29 12:03:45 +0000

    optimizer tests

--- src/test/scala/io/kirill/playlistoptimizer/optimizer/OptimizerTest.scala
+++ src/test/scala/io/kirill/playlistoptimizer/optimizer/OptimizerTest.scala
@@ -1,61 +1,36 @@
 package io.kirill.playlistoptimizer.optimizer
 
-import io.kirill.playlistoptimizer.domain.{Playlist, Song}
-import org.scalatest.{FunSpec, Matchers}
+import io.kirill.playlistoptimizer.domain.TrackBuilder.track
 import io.kirill.playlistoptimizer.domain._
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
 import scala.util.Random
 
-class OptimizerTest extends FunSpec with Matchers {
+class OptimizerTest extends AnyWordSpec with Matchers {
   import Key._
 
   implicit val random = new Random(1)
 
-  describe("Optimizer") {
-    val playlist = Playlist(
-      Song("song1", "artist", BMajor),
-      Song("song2", "artist", EMajor),
-      Song("song3", "artist", EMajor),
-      Song("song4", "artist", AMajor),
-      Song("song5", "artist", DMajor),
-      Song("song6", "artist", GMajor),
-      Song("song7", "artist", GMajor),
-      Song("song8", "artist", GMajor),
-      Song("song9", "artist", GMajor),
-      Song("song10", "artist", GMajor)
-    )
+  val tracks = Vector(
+    track("song 1", BMajor),
+    track("song 2", EMajor),
+    track("song 3", EMajor),
+    track("song 4", AMajor),
+    track("song 5", DMajor),
+    track("song 6", GMajor),
+    track("song 7", GMajor),
+    track("song 8", GMajor),
+    track("song 9", GMajor),
+    track("song 10", GMajor)
+  )
 
-    describe("distributeInPairs") {
-      it("distributes elements in list in pairs") {
-        val list = List(1,2,3,4,5,6,7,8,9)
-        val result = Optimizer.distributeInPairs(list)
-        result should contain theSameElementsInOrderAs (List((1,2), (3,4), (5,6), (7,8)))
-      }
-    }
+  "An Optimizer" should {
 
-    describe("evaluate") {
-      it("evaluates a playlist") {
-        val score = Optimizer.evaluate(playlist)
-        assert(score === 4)
-      }
-    }
+    "create initial population" in {
+      val population = Optimizer.initPopulation(tracks, 10)
 
-    describe("mutate") {
-      it("mutates a playlist") {
-        val mutatedPlaylist = Optimizer.mutate(playlist)
-
-        mutatedPlaylist.songs should not contain theSameElementsInOrderAs (playlist.songs)
-        mutatedPlaylist.songs should contain theSameElementsAs playlist.songs
-      }
-    }
-
-    describe("crossover") {
-      it("crossovers 2 playlists") {
-        val playlist2 = playlist.map(Random.shuffle(_))
-        val child = Optimizer.crossover(playlist, playlist2)
-
-        child.songs should contain theSameElementsAs playlist.songs
-      }
+      population must have size (10)
     }
   }
 }
