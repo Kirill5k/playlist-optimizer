403f87992f5c50260cfeb1a21ca10e7d9e82b308
kirill5k <immotional@aol.com>
2020-03-08 14:08:49 +0000

    neighbourSwapMutator tests and implementation

--- src/main/scala/io/kirill/playlistoptimizer/optimizer/operators/Mutator.scala
+++ src/main/scala/io/kirill/playlistoptimizer/optimizer/operators/Mutator.scala
@@ -10,8 +10,16 @@ sealed trait Mutator[A] {
 
 object Mutator {
   implicit def randomSwapMutator[A](implicit R: Random): Mutator[A] = new Mutator[A] {
-    override def mutate(s: Seq[A]): Seq[A] = {
+    override def mutate(s: Seq[A]): Seq[A] =
       s.swap(R.nextInt(s.size), R.nextInt(s.size))
+  }
+
+  implicit def neighbourSwapMutator[A](implicit R: Random): Mutator[A] = new Mutator[A] {
+    override def mutate(s: Seq[A]): Seq[A] = {
+      val p1 = R.nextInt(s.size)
+      val p2 = if (p1 > s.size/2) p1 - 1 else p1 + 1
+      s.swap(p1, p2)
     }
   }
+
 }
