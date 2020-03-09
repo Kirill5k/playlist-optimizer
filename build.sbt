0dfc72483223ea84d3c77a049dd5fb3fc71e8f12
kirill <immotional@aol.com>
2020-03-09 14:17:08 +0000

    serve index html from resources

--- build.sbt
+++ build.sbt
@@ -7,18 +7,27 @@ scalaVersion := "2.13.1"
 lazy val circeVersion = "0.12.3"
 lazy val mockitoVersion = "1.10.3"
 lazy val sttpVersion = "2.0.0-RC13"
+lazy val http4sVersion = "0.21.1"
 
 libraryDependencies ++= Seq(
   "com.github.pureconfig" %% "pureconfig" % "0.12.2",
+
   "org.typelevel" %% "cats-core" % "2.1.0",
   "org.typelevel" %% "cats-effect" % "2.1.1",
   "co.fs2" %% "fs2-core" % "2.2.2",
+
   "io.circe" %% "circe-core" % circeVersion,
   "io.circe" %% "circe-generic" % circeVersion,
   "io.circe" %% "circe-parser" % circeVersion,
+
   "com.softwaremill.sttp.client" %% "core" % sttpVersion,
   "com.softwaremill.sttp.client" %% "circe" % sttpVersion,
   "com.softwaremill.sttp.client" %% "async-http-client-backend-cats" % sttpVersion,
+
+  "org.http4s" %% "http4s-core" % http4sVersion,
+  "org.http4s" %% "http4s-dsl" % http4sVersion,
+  "org.http4s" %% "http4s-server" % http4sVersion,
+  "org.http4s" %% "http4s-blaze-server" % http4sVersion,
 
   "org.mockito" %% "mockito-scala" % mockitoVersion % Test,
   "org.mockito" %% "mockito-scala-scalatest" % mockitoVersion % Test,
---
+++ src/main/scala/io/kirill/playlistoptimizer/Application.scala
@@ -0,0 +1,32 @@
+package io.kirill.playlistoptimizer
+
+import cats.effect._
+import cats.implicits._
+import org.http4s.dsl.Http4sDsl
+import org.http4s.{HttpRoutes, StaticFile}
+import org.http4s.server.blaze.BlazeServerBuilder
+import org.http4s.server.{Router, Server}
+import org.http4s.syntax.kleisli._
+
+
+object Application extends IOApp with Http4sDsl[IO] {
+  override def run(args: List[String]): IO[ExitCode] =
+    app.use(_ => IO.never).as(ExitCode.Success)
+
+  val app: Resource[IO, Server[IO]] =
+    for {
+      blocker <- Blocker[IO]
+      server <- BlazeServerBuilder[IO].bindHttp(8080).withHttpApp(Router(
+          "" -> staticResourcesRoute(blocker)
+        ).orNotFound).resource
+    } yield server
+
+
+  def staticResourcesRoute(blocker: Blocker): HttpRoutes[IO] =
+    HttpRoutes.of[IO] {
+      case req @ GET -> Root =>
+        StaticFile.fromResource("static/index.html", blocker, Some(req)).getOrElseF(NotFound())
+      case req @ GET -> "static" /: path =>
+        StaticFile.fromResource(path.toString, blocker, Some(req)).getOrElseF(NotFound())
+    }
+}
