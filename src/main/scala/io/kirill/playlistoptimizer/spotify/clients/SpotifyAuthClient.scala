3d2d465f4c9f18a90e74cbaf0e77eebd00ada8e1
kirill <immotional@aol.com>
2020-03-11 11:15:37 +0000

    refactoring of SpotifyAuthApi

--- src/main/scala/io/kirill/playlistoptimizer/spotify/clients/SpotifyAuthClient.scala
+++ src/main/scala/io/kirill/playlistoptimizer/spotify/clients/SpotifyAuthClient.scala
@@ -4,15 +4,24 @@ import java.time.Instant
 
 import cats.effect.IO
 import io.kirill.playlistoptimizer.configs.SpotifyConfig
+import io.kirill.playlistoptimizer.spotify.clients.api.{SpotifyAuthApi, SpotifyRestApi}
 import sttp.client.{NothingT, SttpBackend}
 
 private[clients] case class SpotifyAccessToken(accessToken: String, refreshToken: String, userId: String, expiresAt: Instant) {
   def isValid: Boolean = expiresAt.isBefore(Instant.now())
 }
 
+private[clients] object SpotifyAccessToken {
+  def apply(accessToken: String, refreshToken: String, userId: String, expiresIn: Int): SpotifyAccessToken =
+    new SpotifyAccessToken(accessToken, refreshToken, userId, Instant.now().plusSeconds(expiresIn))
+}
+
 private[spotify] class SpotifyAuthClient(authCode: String)(implicit val sc: SpotifyConfig, val b: SttpBackend[IO, Nothing, NothingT]) {
-
-
+  
+  private var spotifyAccessToken: IO[SpotifyAccessToken] = for {
+    authResponse <- SpotifyAuthApi.authorize(authCode)
+    userResponse <- SpotifyRestApi.getCurrentUser(authResponse.access_token)
+  } yield SpotifyAccessToken(authResponse.access_token, authResponse.refresh_token, userResponse.id, authResponse.expires_in)
 
   def token: IO[String] = ???
 }
--- src/main/scala/io/kirill/playlistoptimizer/spotify/clients/api/SpotifyAuthApi.scala
+++ src/main/scala/io/kirill/playlistoptimizer/spotify/clients/api/SpotifyAuthApi.scala
@@ -2,6 +2,7 @@ package io.kirill.playlistoptimizer.spotify.clients.api
 
 import cats.implicits._
 import cats.MonadError
+import io.circe.Decoder
 import io.circe.generic.auto._
 import io.circe.parser._
 import io.kirill.playlistoptimizer.spotify.clients.api.SpotifyError.SpotifyAuthError
@@ -14,14 +15,21 @@ import sttp.model.MediaType
 object SpotifyAuthApi {
 
   def authorize[F[_]](code: String)(implicit C: SpotifyConfig, B: SttpBackend[F, Nothing, NothingT], M: MonadError[F, Throwable]): F[SpotifyAuthResponse] =
+    getToken[F, SpotifyAuthResponse](Map("grant_type" -> "authorization_code", "code" -> code, "redirect_uri" -> C.auth.redirectUri))
+
+
+  private def getToken[F[_], R <: SpotifyResponse: Decoder](requestBody: Map[String, String])(
+    implicit C: SpotifyConfig, B: SttpBackend[F, Nothing, NothingT], M: MonadError[F, Throwable]
+  ): F[R] =
     basicRequest
-      .body(Map("grant_type" -> "authorization_code", "code" -> code, "redirect_uri" -> C.auth.redirectUri))
+      .body(requestBody)
       .auth.basic(C.auth.clientId, C.auth.clientSecret)
       .contentType(MediaType.ApplicationXWwwFormUrlencoded)
       .post(uri"${C.auth.baseUrl}${C.auth.tokenPath}")
-      .response(asJson[SpotifyAuthResponse])
+      .response(asJson[R])
       .send()
-      .flatMap(r => mapResponseBody[F, SpotifyAuthResponse](r.body))
+      .flatMap(r => mapResponseBody[F, R](r.body))
+
 
   private def mapResponseBody[F[_], R <: SpotifyResponse](responseBody: Either[ResponseError[io.circe.Error], R])(
     implicit m: MonadError[F, Throwable]
