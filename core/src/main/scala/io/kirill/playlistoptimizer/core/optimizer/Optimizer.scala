ccf48fdeee5fda3379c506f1f7d5c739ac7e2d31
kirill5k <immotional@aol.com>
2020-07-26 20:16:01 +0100

    initial refactoring of optimizer

--- core/src/main/scala/io/kirill/playlistoptimizer/core/optimizer/Optimizer.scala
+++ core/src/main/scala/io/kirill/playlistoptimizer/core/optimizer/Optimizer.scala
@@ -1,9 +1,13 @@
 package io.kirill.playlistoptimizer.core.optimizer
 
+import java.util.UUID
+
 import cats.effect._
 import cats.implicits._
 import fs2.Stream
 import io.kirill.playlistoptimizer.core.common.config.GeneticAlgorithmConfig
+import io.kirill.playlistoptimizer.core.optimizer.Optimizer.OptimizationResult
+import io.kirill.playlistoptimizer.core.optimizer.algorithms.Algorithm
 import io.kirill.playlistoptimizer.core.optimizer.operators.{Crossover, Evaluator, Mutator}
 import io.kirill.playlistoptimizer.core.optimizer.operators.Crossover
 import io.kirill.playlistoptimizer.core.utils.CollectionOps._
@@ -11,36 +15,11 @@ import io.kirill.playlistoptimizer.core.utils.CollectionOps._
 import scala.util.Random
 
 trait Optimizer[F[_], A] {
-  def optimize(items: Seq[A])(implicit e: Evaluator[A], r: Random): F[Seq[A]]
+  def optimize(items: Seq[A])(implicit alg: Algorithm[F, A]): F[Seq[A]]
+
+  def get(id: UUID): F[OptimizationResult[A]]
 }
 
 object Optimizer {
-  implicit def geneticAlgorithmOptimizer[F[_]: Concurrent, A: Crossover : Mutator](config: GeneticAlgorithmConfig): Optimizer[F, A] =
-    new Optimizer[F, A] {
-      val popSize = config.populationSize
-      val mutFactor = config.mutationFactor
-      val its = config.iterations
-
-      override def optimize(items: Seq[A])(implicit e: Evaluator[A], r: Random): F[Seq[A]] =
-        Stream.range[F](0, its)
-          .evalScan(items.shuffledCopies(popSize))((currPop, _) => singleIteration(currPop))
-          .compile
-          .lastOrError
-          .map(_.head)
-
-      private def singleIteration(population: Seq[Seq[A]])(implicit e: Evaluator[A], r: Random): F[Seq[Seq[A]]] = {
-        val newPopulation = Stream.evalSeq(Concurrent[F].delay(population.pairs))
-          .map { case (p1, p2) => Stream.evalSeq(Concurrent[F].delay(List(breed(p1, p2), breed(p2, p1)))) }
-          .parJoinUnbounded
-
-        val oldPopulation = Stream.evalSeq(Concurrent[F].pure(population))
-
-        (newPopulation ++ oldPopulation).compile.toList.map(_.sortBy(e.evaluate).take(popSize))
-      }
-
-      private def breed(p1: Seq[A], p2: Seq[A])(implicit c: Crossover[A], m: Mutator[A], r: Random): Seq[A] = {
-        val child = c.cross(p1, p2)
-        if (r.nextDouble < mutFactor) m.mutate(child) else child
-      }
-  }
+  final case class OptimizationResult[A](id: UUID, result: Option[A])
 }
