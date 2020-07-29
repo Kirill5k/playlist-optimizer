892e2c20bd9b3c598f633e881218696a44aceecc
kirill5k <immotional@aol.com>
2020-07-29 14:14:15 +0100

    basic frontend

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,12 +3,31 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const reject = err => Promise.reject(new Error(err))
+
 export default new Vuex.Store({
   state: {
+    isAuthenticated: true,
+    playlists: []
   },
   mutations: {
+    setPlaylists (state, playlists) {
+      state.playlists = playlists
+    },
+    unAuthenticate (state) {
+      state.isAuthenticated = false
+    }
   },
   actions: {
+    getPlaylists ({ commit }) {
+      return fetch('/api/spotify/playlists')
+        .then(res => res.status === 200 ? res.json() : reject(res.status))
+        .then(playlists => commit('setPlaylists', playlists))
+        .catch(err => {
+          console.error(err)
+          commit('unAuthenticate')
+        })
+    }
   },
   modules: {
   }
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <a v-if="!isAuthenticated" href="/api/spotify/login" aria-label="Left Align">
+      <font-awesome-icon :icon="spotifyIcon" size="7x" />
+    </a>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faSpotify } from '@fortawesome/free-brands-svg-icons'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    FontAwesomeIcon
+  },
+  created () {
+    this.$store.dispatch('getPlaylists')
+  },
+  computed: {
+    spotifyIcon () {
+      return faSpotify
+    },
+    isAuthenticated () {
+      return this.$store.state.isAuthenticated
+    }
   }
 }
 </script>
