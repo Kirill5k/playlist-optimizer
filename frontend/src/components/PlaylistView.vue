a1d214cf11aae79f556e1704b1f131cb8c59d437
kirill5k <immotional@aol.com>
2020-07-30 13:46:42 +0100

    added bootstrap

--- frontend/src/components/PlaylistView.vue
+++ frontend/src/components/PlaylistView.vue
@@ -1,14 +1,35 @@
 <template>
   <div>
     {{ playlist.name }}
+    <b-table striped hover :items="tracks" :fields="fields"></b-table>
   </div>
 </template>
 
 <script>
+import { BTable } from 'bootstrap-vue'
+
 export default {
   name: 'PlaylistView',
   props: {
     playlist: Object
+  },
+  components: {
+    BTable
+  },
+  data () {
+    return {
+      fields: ['artists', 'name', 'key', 'mode']
+    }
+  },
+  computed: {
+    tracks () {
+      return this.playlist.tracks.map(t => ({
+        artists: t.artists.join(', '),
+        name: t.name,
+        key: t.key,
+        mode: t.mode
+      }))
+    }
   }
 }
 </script>
