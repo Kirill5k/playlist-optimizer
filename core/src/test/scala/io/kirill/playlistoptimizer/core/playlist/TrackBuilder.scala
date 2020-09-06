df40b990384ff955a043fc5f0538c4a46fa97c9f
kirill5k <immotional@aol.com>
2020-09-06 12:31:20 +0100

    cleanup

--- core/src/test/scala/io/kirill/playlistoptimizer/core/playlist/TrackBuilder.scala
+++ core/src/test/scala/io/kirill/playlistoptimizer/core/playlist/TrackBuilder.scala
@@ -6,10 +6,16 @@ import scala.concurrent.duration.Duration
 
 object TrackBuilder {
 
-  def track(name: String, key: Key, artist: String = "Artist"): Track = {
+  def track(
+      name: String,
+      key: Key,
+      danceability: Double = 0.807,
+      energy: Double = 0.613,
+      artist: String = "Artist"
+  ): Track = {
     val details = SongDetails(name, List(artist), None, None, None)
-    val audio = AudioDetails(140, Duration(3, TimeUnit.MINUTES), key, 0.807, 0.613)
-    val source = SourceDetails(s"file://$artist-$name", None)
+    val audio   = AudioDetails(140, Duration(3, TimeUnit.MINUTES), key, danceability, energy)
+    val source  = SourceDetails(s"file://$artist-$name", None)
     Track(details, audio, source)
   }
 }
