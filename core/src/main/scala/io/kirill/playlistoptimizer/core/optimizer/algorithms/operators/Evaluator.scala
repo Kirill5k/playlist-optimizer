692ae586197fd369c3caa148de3f776b32cb3811
kirill5k <immotional@aol.com>
2020-09-06 20:38:43 +0100

    cleanup

--- core/src/main/scala/io/kirill/playlistoptimizer/core/optimizer/algorithms/operators/Evaluator.scala
+++ core/src/main/scala/io/kirill/playlistoptimizer/core/optimizer/algorithms/operators/Evaluator.scala
@@ -13,28 +13,24 @@ sealed trait Evaluator[A] {
 
 object Evaluator {
   implicit def keyDistanceBasedTracksEvaluator: Evaluator[Track] = new Evaluator[Track] {
-    override def evaluateIndividual(tracks: IndexedSeq[Track]): Fitness = {
-      val score = calcScore[Track](tracks, (prev, curr) => math.pow(Key.distance(prev.audio.key, curr.audio.key), 2))
-      Fitness(BigDecimal(score).setScale(4, BigDecimal.RoundingMode.HALF_UP))
-    }
+    override def evaluateIndividual(tracks: IndexedSeq[Track]): Fitness =
+      calcFitness[Track](tracks, (prev, curr) => math.pow(Key.distance(prev.audio.key, curr.audio.key), 2))
   }
 
   def energyFlowBasedTracksEvaluator: Evaluator[Track] = new Evaluator[Track] {
-    override def evaluateIndividual(individual: IndexedSeq[Track]): Fitness = {
-      val score = calcScore[Track](individual, (prev, curr) => math.abs(prev.audio.energy - curr.audio.energy))
-      Fitness(BigDecimal(score).setScale(4, BigDecimal.RoundingMode.HALF_UP))
-    }
+    override def evaluateIndividual(individual: IndexedSeq[Track]): Fitness =
+      calcFitness[Track](individual, (prev, curr) => math.abs(prev.audio.energy - curr.audio.energy))
   }
 
   def danceabilityBasedTracksEvaluator: Evaluator[Track] = new Evaluator[Track] {
-    override def evaluateIndividual(individual: IndexedSeq[Track]): Fitness = {
-      val score = calcScore[Track](individual, (prev, curr) => math.abs(prev.audio.danceability - curr.audio.danceability))
-      Fitness(BigDecimal(score).setScale(4, BigDecimal.RoundingMode.HALF_UP))
-    }
+    override def evaluateIndividual(individual: IndexedSeq[Track]): Fitness =
+      calcFitness[Track](individual, (prev, curr) => math.abs(prev.audio.danceability - curr.audio.danceability))
   }
 
-  private def calcScore[A](individual: IndexedSeq[A], calculation: (A, A) => Double): Double =
-    individual.tail.foldLeft[(Double, A)]((0, individual.head)) {
+  private def calcFitness[A](individual: IndexedSeq[A], calculation: (A, A) => Double): Fitness = {
+    val score = individual.tail.foldLeft[(Double, A)]((0, individual.head)) {
       case ((acc, prev), curr) => (acc + calculation(prev, curr), curr)
     }._1
+    Fitness(BigDecimal(score).setScale(4, BigDecimal.RoundingMode.HALF_UP))
+  }
 }
