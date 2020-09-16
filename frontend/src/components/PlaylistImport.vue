30078eceb7e5d646e89cb164c087b4230979feb2
kirill5k <immotional@aol.com>
2020-09-16 09:01:37 +0100

    playlist import modal styles

--- frontend/src/components/PlaylistImport.vue
+++ frontend/src/components/PlaylistImport.vue
@@ -9,21 +9,77 @@
       Import
     </b-button>
 
-    <b-modal ref="my-modal" title="Enter playlist details">
-      <div class="d-block text-center">
-        <h3>Hello From My Modal!</h3>
-      </div>
+    <b-modal
+      size="sm"
+      ref="my-modal"
+      title="New playlist"
+      content-class="playlist-import__import-modal"
+    >
+      <b-form>
+        <b-form-group
+          id="playlist-name-group"
+          label="Playlist name:"
+          label-for="playlist-name"
+          label-size="sm"
+          label-class="mb-0"
+        >
+          <b-form-input
+            id="playlist-name"
+            v-model="name"
+            type="text"
+            required
+            placeholder="My new playlist"
+            size="sm"
+            trim
+          />
+        </b-form-group>
+
+        <b-form-group
+          id="playlist-description-group"
+          label="Playlist description:"
+          label-for="playlist-description"
+          label-size="sm"
+          label-class="mb-0"
+        >
+          <b-form-input
+            id="playlist-description"
+            v-model="description"
+            placeholder="Manually imported playlist"
+            size="sm"
+            trim
+          />
+        </b-form-group>
+
+        <b-form-group
+          id="playlist-tracks-group"
+          label="Playlist tracks:"
+          label-for="playlist-tracks"
+          label-size="sm"
+          label-class="mb-0"
+        >
+          <b-form-textarea
+            id="playlist-tracks"
+            v-model="tracks"
+            rows="9"
+            size="sm"
+            no-resize
+            no-auto-shrink
+            required
+            trim
+          />
+        </b-form-group>
+      </b-form>
 
       <template v-slot:modal-footer="{ ok, hide }">
-        <b-button class="mt-3" variant="primary" @click="ok()">Save</b-button>
-        <b-button class="mt-3" variant="secondary" @click="hide()">Close</b-button>
+        <b-button size="sm" class="mt-3" variant="primary" @click="ok()">Save</b-button>
+        <b-button size="sm" class="mt-3" variant="secondary" @click="hide()">Close</b-button>
       </template>
     </b-modal>
   </div>
 </template>
 
 <script>
-import { BButton, BModal } from 'bootstrap-vue'
+import { BButton, BModal, BForm, BFormGroup, BFormInput, BFormTextarea } from 'bootstrap-vue'
 
 export default {
   name: 'PlaylistImport',
@@ -31,7 +87,14 @@ export default {
     track: Object
   },
   components: {
-    BButton, BModal
+    BButton, BModal, BForm, BFormGroup, BFormInput, BFormTextarea
+  },
+  data () {
+    return {
+      name: '',
+      description: '',
+      tracks: ''
+    }
   },
   methods: {
     showModal () {
@@ -50,6 +113,10 @@ export default {
   &__import-button {
     float: right;
     border: 1px solid #000;
+  }
+
+  &__import-modal {
+    min-width: 600px;
   }
 }
 </style>
