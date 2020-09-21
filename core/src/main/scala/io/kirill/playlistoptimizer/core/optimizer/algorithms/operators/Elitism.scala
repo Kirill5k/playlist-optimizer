3a5f73a790a79f288244bf27db57a7810c5042f1
kirill5k <immotional@aol.com>
2020-09-21 17:38:35 +0100

    elitism refactor

--- core/src/main/scala/io/kirill/playlistoptimizer/core/optimizer/algorithms/operators/Elitism.scala
+++ core/src/main/scala/io/kirill/playlistoptimizer/core/optimizer/algorithms/operators/Elitism.scala
@@ -9,8 +9,8 @@ trait Elitism[A] {
 object Elitism {
   def elitism[A]: Elitism[A] = new Elitism[A] {
     override def select(population: Seq[(IndexedSeq[A], Fitness)], elitismRatio: Double): Seq[IndexedSeq[A]] = {
-      val n = population.size * elitismRatio
-      population.toVector.sortBy(_._2.value).map(_._1).take(n.toInt)
+      val n = math.round(population.size * elitismRatio)
+      population.sortBy(_._2.value).map(_._1).take(n.toInt)
     }
   }
 }
