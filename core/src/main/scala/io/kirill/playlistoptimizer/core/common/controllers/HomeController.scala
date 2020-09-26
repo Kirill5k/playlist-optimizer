9767c85d2cc42c47f5d373acfade9383f02f4de2
kirill5k <immotional@aol.com>
2020-09-26 15:49:50 +0100

    server txt files

--- core/src/main/scala/io/kirill/playlistoptimizer/core/common/controllers/HomeController.scala
+++ core/src/main/scala/io/kirill/playlistoptimizer/core/common/controllers/HomeController.scala
@@ -1,14 +1,13 @@
 package io.kirill.playlistoptimizer.core.common.controllers
 
 import cats.effect._
-import cats.implicits._
 import io.chrisdavenport.log4cats.Logger
 import org.http4s.{HttpRoutes, StaticFile}
 import java.io.File
 
 private[controllers] class HomeController[F[_]](blocker: Blocker) extends AppController[F] {
 
-  private val expectedFiles = List(".ico", ".svg", ".png", ".json", ".js", ".css", ".map", ".html", ".webm")
+  private val expectedFiles = List(".txt", ".ico", ".svg", ".png", ".json", ".js", ".css", ".map", ".html", ".webm")
 
   override def routes(implicit cs: ContextShift[F], s: Sync[F], l: Logger[F]): HttpRoutes[F] =
     HttpRoutes.of[F] {
