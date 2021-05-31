938b246f2cdcbc9e6836d6832353ffb321af73f4
kirill5k <immotional@aol.com>
2021-05-31 10:26:52 +0100

    rename fitness calculation

--- core/src/main/scala/io/kirill/playlistoptimizer/core/optimizer/algorithms/operators/Evaluator.scala
+++ core/src/main/scala/io/kirill/playlistoptimizer/core/optimizer/algorithms/operators/Evaluator.scala
@@ -8,12 +8,12 @@ sealed trait FitnessCalculation[A] {
   def evaluate(ind1: A, ind2: A): Double
 }
 
-object HarmonicSeqBasedTracksEvaluator extends FitnessCalculation[Track] {
+object HarmonicSeqBasedTracksFitnessCalculation extends FitnessCalculation[Track] {
   override def evaluate(ind1: Track, ind2: Track): Double =
     math.pow(Key.distance(ind1.audio.key, ind2.audio.key).toDouble, 2.0)
 }
 
-object EnergyFlowBasedTracksEvaluator extends FitnessCalculation[Track] {
+object EnergyFlowBasedTracksFitnessCalculation extends FitnessCalculation[Track] {
   override def evaluate(ind1: Track, ind2: Track): Double = {
     val energy       = math.abs(ind1.audio.energy * 1000 - ind2.audio.energy * 1000)
     val danceability = math.abs(ind1.audio.danceability * 1000 - ind2.audio.danceability * 1000)
@@ -41,11 +41,11 @@ trait Evaluator[A] {
 object Evaluator {
   implicit def harmonicSeqBasedTracksEvaluator: Evaluator[Track] = new Evaluator[Track] {
     override def evaluateIndividual(tracks: IndexedSeq[Track]): Fitness =
-      calcFitness(tracks)(HarmonicSeqBasedTracksEvaluator)
+      calcFitness(tracks)(HarmonicSeqBasedTracksFitnessCalculation)
   }
 
   def energyFlowBasedTracksEvaluator: Evaluator[Track] = new Evaluator[Track] {
     override def evaluateIndividual(individual: IndexedSeq[Track]): Fitness =
-      calcFitness(individual)(EnergyFlowBasedTracksEvaluator)
+      calcFitness(individual)(EnergyFlowBasedTracksFitnessCalculation)
   }
 }
