521ab39d42304db944cf298e930dfa8073d7668f
kirill5k <immotional@aol.com>
2022-01-20 16:36:41 +0200

    Add opt params

--- free/src/main/scala/io/kirill/playlistoptimizer/free/Algorithm.scala
+++ free/src/main/scala/io/kirill/playlistoptimizer/free/Algorithm.scala
@@ -1,24 +1,33 @@
 package io.kirill.playlistoptimizer.free
 
 trait Algorithm:
-  def optimize[G](target: Ind[G]): Op[Ind[G]]
+  def optimize[G](target: Ind[G], params: Algorithm.OptimizationParameters): Op[Ind[G]]
 
 object Algorithm:
+  final case class OptimizationParameters(
+      populationSize: Int,
+      maxGen: Int,
+      crossoverProbability: Double,
+      mutationProbability: Double,
+      elitismRatio: Double,
+      shuffle: Boolean
+  )
+
   val genAlg = new Algorithm {
-    override def optimize[G](target: Ind[G]): Op[Ind[G]] = {
+    override def optimize[G](target: Ind[G], params: OptimizationParameters): Op[Ind[G]] = {
       val singleIteration: (pop: Population[G]) => Op[Population[G]] = pop =>
         for
           evPop    <- Op.EvaluatePopulation(pop)
-          elites   <- Op.SelectElites(evPop)
-          pairs    <- Op.SelectPairs(evPop)
-          crossed1 <- Op.Traverse(pairs, (i1, i2) => Op.Crossover(i1, i2))
-          crossed2 <- Op.Traverse(pairs, (i1, i2) => Op.Crossover(i2, i1))
-          mutated  <- Op.Traverse(crossed1 ++ crossed2, i => Op.Mutate(i))
+          elites   <- Op.SelectElites(evPop, params.populationSize, params.elitismRatio)
+          pairs    <- Op.SelectPairs(evPop, params.populationSize)
+          crossed1 <- Op.Traverse(pairs, (i1, i2) => Op.Crossover(i1, i2, params.crossoverProbability))
+          crossed2 <- Op.Traverse(pairs, (i1, i2) => Op.Crossover(i2, i1, params.crossoverProbability))
+          mutated  <- Op.Traverse(crossed1 ++ crossed2, i => Op.Mutate(i, params.mutationProbability))
         yield mutated ++ elites
 
       for
-        pop      <- Op.InitPopulation(target)
-        finalPop <- Op.Iterate(pop, singleIteration)
+        pop      <- Op.InitPopulation(target, params.populationSize, params.shuffle)
+        finalPop <- Op.Iterate(pop, params.maxGen, singleIteration)
         evPop    <- Op.EvaluatePopulation(finalPop)
         fittest  <- Op.SelectFittest(evPop)
       yield fittest
--- free/src/main/scala/io/kirill/playlistoptimizer/free/Op.scala
+++ free/src/main/scala/io/kirill/playlistoptimizer/free/Op.scala
@@ -8,19 +8,19 @@ type EvaluatedPopulation   = [G] =>> List[(Ind[G], Fitness)]
 type DistributedPopulation = [G] =>> List[(Ind[G], Ind[G])]
 
 enum Op[A]:
-  case InitPopulation[G](seed: Ind[G]) extends Op[Population[G]]
-  case Crossover[G](ind1: Ind[G], ind2: Ind[G]) extends Op[Ind[G]]
-  case Mutate[G](ind: Ind[G]) extends Op[Ind[G]]
+  case InitPopulation[G](seed: Ind[G], size: Int, shuffle: Boolean) extends Op[Population[G]]
+  case Crossover[G](ind1: Ind[G], ind2: Ind[G], prob: Double) extends Op[Ind[G]]
+  case Mutate[G](ind: Ind[G], prob: Double) extends Op[Ind[G]]
   case EvaluateOne[G](ind: Ind[G]) extends Op[(Ind[G], Fitness)]
   case EvaluatePopulation[G](population: Population[G]) extends Op[EvaluatedPopulation[G]]
-  case SelectElites[G](population: EvaluatedPopulation[G]) extends Op[Population[G]]
-  case SelectPairs[G](population: EvaluatedPopulation[G]) extends Op[DistributedPopulation[G]]
+  case SelectElites[G](population: EvaluatedPopulation[G], popSize: Int, ratio: Double) extends Op[Population[G]]
+  case SelectPairs[G](population: EvaluatedPopulation[G], limit: Int) extends Op[DistributedPopulation[G]]
   case SelectFittest[G](population: EvaluatedPopulation[G]) extends Op[Ind[G]]
 
   case Pure[A](value: A) extends Op[A]
   case Bind[A, B](op: Op[A], f: A => Op[B]) extends Op[B]
   case Traverse[A, B](ops: List[A], f: A => Op[B]) extends Op[List[B]]
-  case Iterate[A](a: A, f: A => Op[A]) extends Op[A]
+  case Iterate[A](a: A, n: Int, f: A => Op[A]) extends Op[A]
 
   def flatMap[B](f: A => Op[B]): Op[B] = Bind(this, f)
   def map[B](f: A => B): Op[B]         = Bind(this, a => Pure(f(a)))
